0bdc52d18a8a65904257b5ba02868f2639ce0679
junghan92 <109567001+junghan92@users.noreply.github.com>
2022-11-06 22:44:45 +0900

    병합 빌드

--- src/pages/Class.tsx
+++ src/pages/Class.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-} from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {Text} from 'react-native';
 
 function Class() {
-  //구글소셜로그인
-  const onGoogleButtonPress = async () => {
-    const {idToken} = await GoogleSignin.signIn();
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    return auth().signInWithCredential(googleCredential);
-  };
   return (
     <SafeAreaView>
-      <GoogleSigninButton onPress={() => onGoogleButtonPress()} />
+      <Text>모임</Text>
     </SafeAreaView>
   );
 }
--- src/pages/MyFeed.tsx
+++ src/pages/MyFeed.tsx
@@ -1,16 +1,29 @@
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
 import React, {useState} from 'react';
+import {login} from '@react-native-seoul/kakao-login';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {
-  login,
-  logout,
-  getProfile as getKakaoProfile,
-  unlink,
-} from '@react-native-seoul/kakao-login';
-
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import NaverLogin, {
+  GetProfileResponse,
+  NaverLoginResponse,
+} from '@react-native-seoul/naver-login';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 const Intro = () => {
   const [result, setResult] = useState<string>('');
   console.log(result);
-
+  const [success, setSuccessResponse] =
+    useState<NaverLoginResponse['successResponse']>();
+  const [failure, setFailureResponse] =
+    useState<NaverLoginResponse['failureResponse']>();
+  const consumerKey = 'lpKmvVpKYtGlS9D8qI6j';
+  const consumerSecret = 'K7PbKFcaPM';
+  const appName = 'com.aniwalk';
+  const serviceUrlScheme = 'aniwalk';
+  // 카카오 로그인
   const signInWithKakao = async (): Promise<void> => {
     try {
       const token = await login();
@@ -20,82 +33,109 @@ const Intro = () => {
       console.error('login err', err);
     }
   };
-
-  const signOutWithKakao = async (): Promise<void> => {
-    try {
-      const message = await logout();
-
-      setResult(message);
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error('signOut error', err);
-    }
+  // 네이버 로그인
+  const naverLogin = async () => {
+    const {failureResponse, successResponse} = await NaverLogin.login({
+      appName,
+      consumerKey,
+      consumerSecret,
+      serviceUrlScheme,
+    });
+    setSuccessResponse(successResponse);
+    console.log(successResponse);
+    setFailureResponse(failureResponse);
   };
-
-  const getProfile = async (): Promise<void> => {
-    try {
-      const profile = await getKakaoProfile();
-
-      setResult(JSON.stringify(profile));
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error('signOut error', err);
-    }
+  //구글소셜로그인
+  const onGoogleButtonPress = async () => {
+    const {idToken} = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    return auth().signInWithCredential(googleCredential);
   };
-
-  const unlinkKakao = async (): Promise<void> => {
-    try {
-      const message = await unlink();
-
-      setResult(message);
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error('signOut error', err);
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <Pressable
-        style={styles.button}
-        onPress={() => {
-          signInWithKakao();
+    <SafeAreaView style={styles.container}>
+      <View style={styles.kakaoBox}>
+        <Pressable
+          onPress={() => {
+            signInWithKakao();
+          }}>
+          <Image
+            source={require('../assets/image/login/kakao_login.png')}
+            style={styles.kakaoIcon}
+          />
+        </Pressable>
+      </View>
+      <View style={styles.naverBox}>
+        <Pressable
+          onPress={() => {
+            naverLogin();
+          }}
+          style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Image
+            source={require('../assets/image/login/naver_icon.png')}
+            style={styles.naverLogin}
+          />
+          <Text style={{fontSize: wp(4), marginLeft: wp(21), marginTop: hp(1)}}>
+            네이버 로그인
+          </Text>
+        </Pressable>
+      </View>
+      <View
+        style={{
+          marginTop: hp(2),
+          marginLeft: wp(10),
+          width: wp(80),
+          height: hp(5),
+          backgroundColor: '#FFFFFF',
+          borderRadius: 12,
+          justifyContent: 'center',
         }}>
-        <Text style={styles.text}>카카오 로그인</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={() => getProfile()}>
-        <Text style={styles.text}>프로필 조회</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={() => unlinkKakao()}>
-        <Text style={styles.text}>링크 해제</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={() => signOutWithKakao()}>
-        <Text style={styles.text}>카카오 로그아웃</Text>
-      </Pressable>
-    </View>
+        <Pressable
+          onPress={() => onGoogleButtonPress()}
+          style={{
+            flexDirection: 'row',
+            marginLeft: wp(3),
+          }}>
+          <Image
+            source={require('../assets/image/login/google.png')}
+            style={styles.googleLogin}
+          />
+          <Text style={{fontSize: wp(4), marginLeft: wp(24)}}>구글 로그인</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
-
 export default Intro;
-
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingBottom: 100,
+    flex: 1,
   },
-  button: {
-    backgroundColor: '#FEE500',
-    borderRadius: 40,
-    borderWidth: 1,
-    width: 250,
-    height: 40,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginTop: 10,
+  kakaoBox: {
+    marginTop: wp(80),
+    marginLeft: wp(10),
   },
-  text: {
-    textAlign: 'center',
+  naverBox: {
+    width: wp(80),
+    height: hp(5),
+    backgroundColor: '#03C75A',
+    marginLeft: wp(10),
+    marginTop: wp(3),
+    justifyContent: 'space-between',
+    borderRadius: 12,
+  },
+  kakaoIcon: {
+    width: wp(80),
+    height: hp(5),
+    borderRadius: 12,
+  },
+  naverLogin: {
+    width: wp(60 / 6),
+    height: hp(3),
+    marginTop: hp(1),
+    marginLeft: wp(1),
+  },
+  googleLogin: {
+    width: 20,
+    height: 20,
   },
 });
